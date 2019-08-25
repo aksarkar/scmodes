5dd779e204d12d404313af72777010ba5f9405f2
Abhishek Sarkar <aksarkar@alum.mit.edu>
2019-08-25 13:06:21 -0500

    Figure out why glmpca (R implementation) fails to converge

--- scmodes/benchmark/lra.py
+++ scmodes/benchmark/lra.py
@@ -8,6 +8,7 @@ import scipy.sparse as ss
 import scipy.stats as st
 import scmodes
 import sklearn.decomposition
+import wlra
 
 rpy2.robjects.numpy2ri.activate()
 rpy2.robjects.pandas2ri.activate()
@@ -24,15 +25,32 @@ def training_score_lda(x, n_components=10, learning_method='online', batch_size=
   lam = (L / L.sum(axis=0)).dot(F)
   return st.poisson(mu=lam).logpmf(x).sum()
 
-def training_score_glmpca(x, n_components=10):
-  # We use samples x genes, but GLM-PCA expects genes x samples
-  res = glmpca.glmpca(x.values.T, L=n_components, fam='poi')
-  # Follow GLM-PCA code here, not the paper
-  s = np.log(x.values.sum(axis=0) / x.mean(axis=1).sum()).reshape(-1, 1)
-  L = np.array(res.rx2('loadings'))
-  F = np.array(res.rx2('factors'))
-  lam = np.exp(s.reshape(1, -1) + F.T.dot(L))
-  return st.poisson(mu=lam).logpmf(x.values).sum()
+def training_score_glmpca(x, n_components=10, max_restarts=10):
+  # GLMPCA can fail for some (random) initializations, so restart to find one
+  # which works
+  obj = -np.inf
+  for i in range(max_restarts):
+    try:
+      # We use samples x genes, but GLM-PCA expects genes x samples
+      res = glmpca.glmpca(x.values.T, L=n_components, fam='poi')
+      # Follow GLM-PCA code here, not the paper
+      s = np.log(x.values.sum(axis=1))
+      L = np.array(res.rx2('loadings'))
+      F = np.array(res.rx2('factors'))
+      lam = np.exp(s.reshape(1, -1) + F.T.dot(L))
+      llik = st.poisson(mu=lam).logpmf(x.values).sum()
+      if llik > obj:
+        obj = llik
+    except:
+      continue
+  if not np.isfinite(obj):
+    # Figure out what to do with this outside of the benchmark
+    raise RuntimeError('glmpca failed to converge')
+  return obj
+
+def training_score_plra1(x, n_components=10):
+  res = wlra.plra(x.values, rank=n_components, max_iters=50000)
+  return st.poisson(mu=np.exp(res)).logpmf(x.values).sum()
 
 def pois_llik(lam, train, test):
   if ss.issparse(train):
@@ -74,14 +92,31 @@ def generalization_score_lda(train, test, n_components=10, learning_method='onli
   return pois_llik(lam, train, test)
 
 def generalization_score_glmpca(train, test, n_components=10):
-  # We use samples x genes, but GLM-PCA expects genes x samples
-  res = glmpca.glmpca(train.values.T, L=n_components, fam='poi')
-  # Follow GLM-PCA code here, not the paper
-  s = np.log(test.values.sum(axis=0) / test.mean(axis=1).sum()).reshape(-1, 1)
-  L = np.array(res.rx2('loadings'))
-  F = np.array(res.rx2('factors'))
-  lam = np.exp(s.reshape(1, -1) + F.T.dot(L))
-  return st.poisson(mu=lam).logpmf(test.values).sum()
+  # GLMPCA can fail for some (random) initializations, so restart to find one
+  # which works
+  obj = -np.inf
+  for i in range(max_restarts):
+    try:
+      # We use samples x genes, but GLM-PCA expects genes x samples
+      res = glmpca.glmpca(train.values.T, L=n_components, fam='poi')
+      # Follow GLM-PCA code here, not the paper
+      s = np.log(test.values.sum(axis=1))
+      L = np.array(res.rx2('loadings'))
+      F = np.array(res.rx2('factors'))
+      lam = np.exp(s.reshape(1, -1) + F.T.dot(L))
+      llik = st.poisson(mu=lam).logpmf(test.values).sum()
+      if llik > obj:
+        obj = llik
+    except:
+      continue
+  if not np.isfinite(obj):
+    # Figure out what to do with this outside of the benchmark
+    raise RuntimeError('glmpca failed to converge')
+  return obj
+
+def generalization_score_plra1(train, test, n_components=10):
+  res = wlra.plra(train.values, rank=n_components, max_iters=50000)
+  return st.poisson(mu=np.exp(res)).logpmf(test.values).sum()
 
 def evaluate_lra_generalization(x, methods, n_trials=1):
   result = collections.defaultdict(list)
@@ -90,16 +125,10 @@ def evaluate_lra_generalization(x, methods, n_trials=1):
     result[('validation', method)] = []
     for trial in range(n_trials):
       train, val = scmodes.benchmark.train_test_split(x)
-      try:
-        training_score = getattr(scmodes.benchmark, f'training_score_{method}')(train)
-        result[('train', method)].append(training_score)
-      except:
-        result[('train', method)].append(np.nan)
-      try:
-        validation_score = getattr(scmodes.benchmark, f'generalization_score_{method}')(train, val)
-        result[('validation', method)].append(validation_score)
-      except:
-        result[('validation', method)].append(np.nan)
+      training_score = getattr(scmodes.benchmark, f'training_score_{method}')(train)
+      result[('train', method)].append(training_score)
+      validation_score = getattr(scmodes.benchmark, f'generalization_score_{method}')(train, val)
+      result[('validation', method)].append(validation_score)
   result = pd.DataFrame.from_dict(result)
   result.index.name = 'trial'
   return result
