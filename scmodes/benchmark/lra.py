402a05a138ff330ff10e8848e2972213743858c0
Abhishek Sarkar <aksarkar@alum.mit.edu>
2019-08-27 23:40:26 -0500

    Fix handling of GLMPCA

--- scmodes/benchmark/lra.py
+++ scmodes/benchmark/lra.py
@@ -18,36 +18,33 @@ def training_score_nmf(x, n_components=10, **kwargs):
   m = sklearn.decomposition.NMF(n_components=n_components, solver='mu', beta_loss=1).fit(x)
   return st.poisson(mu=m.transform(x).dot(m.components_)).logpmf(x).sum()
 
-def training_score_lda(x, n_components=10, learning_method='online', batch_size=100, **kwargs):
-  model = sklearn.decomposition.LatentDirichletAllocation(n_components=n_components, learning_method=learning_method, batch_size=batch_size, **kwargs)
-  L = model.fit_transform(x)
-  F = model.components_
-  lam = (L / L.sum(axis=0)).dot(F)
-  return st.poisson(mu=lam).logpmf(x).sum()
-
-def training_score_glmpca(x, n_components=10, max_restarts=1, **kwargs):
+def _glmpca(x, n_components, max_restarts):
   # GLMPCA can fail for some (random) initializations, so restart to find one
   # which works
-  obj = -np.inf
+  obj = None
   for i in range(max_restarts):
     try:
-      print(f'GLMPCA {i}')
       # We use samples x genes, but GLM-PCA expects genes x samples
       res = glmpca.glmpca(x.values.T, L=n_components, fam='poi')
       # Follow GLM-PCA code here, not the paper
-      s = np.log(x.values.sum(axis=1))
+      s = np.log(x.values.mean(axis=1, keepdims=True))
       L = np.array(res.rx2('loadings'))
       F = np.array(res.rx2('factors'))
-      lam = np.exp(s.reshape(1, -1) + F.T.dot(L))
+      lam = np.exp(s + F.T.dot(L))
       llik = st.poisson(mu=lam).logpmf(x.values).sum()
-      if llik > obj:
+      print(f'glmpca {i} {llik:.3g}')
+      if obj is None or llik > obj:
         obj = llik
     except:
+      print(f'glmpca {i} failed')
       continue
-  if not np.isfinite(obj):
-    # Figure out what to do with this outside of the benchmark
-    raise RuntimeError('glmpca failed to converge')
-  return obj
+  if obj is None:
+    obj = np.nan
+  return s, L, F, obj
+
+def training_score_glmpca(x, n_components=10, max_restarts=1, **kwargs):
+  res = _glmpca(x, n_components, max_restarts)
+  return res[-1]
 
 def training_score_plra1(x, n_components=10, **kwargs):
   res = wlra.plra(x.values, rank=n_components, max_iters=50000)
@@ -84,36 +81,14 @@ def generalization_score_nmf(train, test, n_components=10, **kwargs):
   m = sklearn.decomposition.NMF(n_components=n_components, solver='mu', beta_loss=1).fit(train)
   return pois_llik(m.transform(train).dot(m.components_), train, test)
 
-def generalization_score_lda(train, test, n_components=10, learning_method='online', batch_size=100, **kwargs):
-  import sklearn.decomposition
-  model = sklearn.decomposition.LatentDirichletAllocation(n_components=n_components, learning_method=learning_method, batch_size=batch_size, **kwargs)
-  L = model.fit_transform(train)
-  F = model.components_
-  lam = (L / L.sum(axis=0)).dot(F)
-  return pois_llik(lam, train, test)
-
 def generalization_score_glmpca(train, test, n_components=10, max_restarts=1, **kwargs):
-  # GLMPCA can fail for some (random) initializations, so restart to find one
-  # which works
-  obj = -np.inf
-  for i in range(max_restarts):
-    try:
-      # We use samples x genes, but GLM-PCA expects genes x samples
-      res = glmpca.glmpca(train.values.T, L=n_components, fam='poi')
-      # Follow GLM-PCA code here, not the paper
-      s = np.log(test.values.sum(axis=1))
-      L = np.array(res.rx2('loadings'))
-      F = np.array(res.rx2('factors'))
-      lam = np.exp(s.reshape(1, -1) + F.T.dot(L))
-      llik = st.poisson(mu=lam).logpmf(test.values).sum()
-      if llik > obj:
-        obj = llik
-    except:
-      continue
-  if not np.isfinite(obj):
-    # Figure out what to do with this outside of the benchmark
-    raise RuntimeError('glmpca failed to converge')
-  return obj
+  s, L, F, llik = _glmpca(train, n_components, max_restarts)
+  if np.isnan(llik):
+    print(f'glmpca failed on training data')
+  # Follow GLM-PCA paper here
+  s1 = np.log(test.values.mean(axis=1, keepdims=True))
+  lam = np.exp(s1 - s + F.T.dot(L))
+  return st.poisson(mu=lam).logpmf(test.values).sum()
 
 def generalization_score_plra1(train, test, n_components=10, **kwargs):
   res = wlra.plra(train.values, rank=n_components, max_iters=50000)
