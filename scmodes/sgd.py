110ccbd3c14e13a2393f4f10cd858cf56988109d
Abhishek Sarkar <aksarkar@alum.mit.edu>
2019-11-20 22:24:22 -0600

    Implement SGD for Poisson--(point-)Gamma

---
+++ scmodes/sgd.py
@@ -0,0 +1,90 @@
+"""Poisson--(point-)Gamma model of scRNA-seq data at a single gene
+
+Under this model, the marginal distribution of x_ij is (ZI)NB. We estimate
+(pi_j, mu_j, phi_j) by maximizing the log likelihood using accelerated SGD.
+
+"""
+import torch
+
+def _nb_llik(x, s, log_mean, log_inv_disp):
+  """Return ln p(x_i | s_i, g)
+
+  x_i ~ Poisson(s_i lambda_i)
+  lambda_i ~ g = Gamma(exp(log_inv_disp), exp(log_mean - log_inv_disp))
+
+  x - [n, p] tensor
+  s - [n, 1] tensor
+  log_mean - [1, p] tensor
+  log_inv_disp - [1, p] tensor
+
+  """
+  mean = torch.matmul(s, torch.exp(log_mean))
+  inv_disp = torch.exp(log_inv_disp)
+  return (x * torch.log(mean / inv_disp)
+          - x * torch.log(1 + mean / inv_disp)
+          - inv_disp * torch.log(1 + mean / inv_disp)
+          # Important: these terms are why we use inverse dispersion
+          + torch.lgamma(x + inv_disp)
+          - torch.lgamma(inv_disp)
+          - torch.lgamma(x + 1))
+
+class PoissonGamma(torch.nn.Module):
+  def __init__(self, p):
+    super().__init__()
+    self.log_mean = torch.zeros([1, p], requires_grad=True)
+    self.log_inv_disp = torch.zeros([1, p], requires_grad=True)
+
+  def forward(self, x, s):
+    """Return negative log likelihood of the data
+
+    x_i ~ Poisson(s_i lambda_i)
+    lambda_i ~ g = Gamma(exp(log_inv_disp), exp(log_mean - log_inv_disp))
+
+    """
+    return -_nb_llik(x, s, self.log_mean, self.log_inv_disp).sum()
+
+  def fit(self, data, max_epochs=10, verbose=False, **kwargs):
+    """Fit the model.
+
+    data - torch.utils.data.DataLoader
+    kwargs - arguments to torch.optim.RMSprop
+
+    """
+    if torch.cuda.is_available():
+      # Move the model to the GPU
+      self.cuda()
+    opt = torch.optim.RMSprop([self.log_mean, self.log_inv_disp], **kwargs)
+    for epoch in range(max_epochs):
+      for (x, s) in data:
+        opt.zero_grad()
+        if torch.cuda.is_available():
+          # Move the data to the GPU
+          x.cuda()
+          s.cuda()
+        loss = self.forward(x, s)
+        loss.backward()
+        opt.step()
+      if verbose:
+        print(loss)
+
+  @torch.no_grad()
+  def opt(self):
+    """Return ln mu_j, ln phi_j"""
+    return self.log_mean.detach().numpy(), -self.log_inv_disp.detach().numpy()
+
+class PoissonPointGamma(PoissonGamma):
+  def __init__(self, p):
+    super().__init__(p)
+    self.logodds = torch.zeros([p, 1])
+
+  def forward(self, x, s):
+    """Return sum of ln p(x_i | s_i g)
+
+    x_i ~ Poisson(s_i lambda_i)
+    lambda_i ~ g = logit(logodds) delta_0() + logit(-logodds) Gamma(exp(log_inv_disp), exp(log_mean - log_inv_disp))
+
+    """
+    nb_llik = _nb_llik(x, s, self.log_mean, self.log_inv_disp)
+    case_zero = -torch.nn.softplus(-logodds) + torch.nn.softplus(nb_llik - logodds)
+    case_non_zero = -torch.nn.softplus(logodds) + nb_llik
+    return torch.where(torch.lt(x, 1), case_zero, case_non_zero).sum()
---
+++ scmodes/tests/test_sgd.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pytest
+import scipy.stats as st
+import scmodes.sgd
+import torch
+import torch.utils.data
+
+@pytest.fixture
+def simulate_gamma():
+  n = 500
+  p = 10
+  np.random.seed(0)
+  # Typical values (Sarkar et al. PLoS Genet 2019)
+  log_mu = np.random.uniform(-12, -6, size=(1, p))
+  log_phi = np.random.uniform(-6, 0, size=(1, p))
+  s = np.random.poisson(lam=1e5, size=(n, 1))
+  # Important: NB success probability is (n, p)
+  F = st.nbinom(n=np.exp(-log_phi), p=1 / (1 + s.dot(np.exp(log_mu + log_phi))))
+  x = F.rvs()
+  llik = F.logpmf(x).sum()
+  return x, s, log_mu, log_phi, llik
+
+def test__nb_llik(simulate_gamma):
+  x, s, log_mu, log_phi, oracle_llik = simulate_gamma
+  llik = scmodes.sgd._nb_llik(torch.tensor(x, dtype=torch.float),
+                              torch.tensor(s, dtype=torch.float),
+                              torch.tensor(log_mu, dtype=torch.float),
+                              torch.tensor(-log_phi, dtype=torch.float)).sum()
+  assert np.isclose(oracle_llik, llik)
+
+def test_PoissonGamma():
+  p = 10
+  m = scmodes.sgd.PoissonGamma(p)
+  log_mu, log_phi = m.opt()
+  assert log_mu.shape == (1, p)
+  assert log_phi.shape == (1, p)
+
+def test_PoissonGamma_fit(simulate_gamma):
+  x, s, log_mu, log_phi, oracle_llik = simulate_gamma
+  n, p = x.shape
+  D = torch.utils.data
+  # Important: data must be float
+  loader = D.DataLoader(
+    D.TensorDataset(torch.tensor(x, dtype=torch.float), torch.tensor(s, dtype=torch.float)),
+    batch_size=1, pin_memory=True)
+  m = scmodes.sgd.PoissonGamma(p)
+  m.fit(loader, max_epochs=10)
+
+  log_mu_hat, log_phi_hat = m.opt()
+  assert log_mu_hat.shape == (1, p)
+  assert log_phi_hat.shape == (1, p)
+  llik = st.nbinom(n=np.exp(-log_phi_hat), p=1 / (1 + s.dot(np.exp(log_mu_hat + log_phi_hat)))).logpmf(x).sum()
+  assert llik > oracle_llik
