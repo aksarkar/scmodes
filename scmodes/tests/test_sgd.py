Fix learning parameters for gradient descent test cases

scmodes/tests/test_sgd.py
@@ -35,7 +35,41 @@ def test_PoissonGamma():
   assert log_mu.shape == (1, p)
   assert log_phi.shape == (1, p)
 
-def test_PoissonGamma_fit(simulate_gamma):
+def test_PoissonGamma_batch(simulate_gamma):
+  x, s, log_mu, log_phi, l0 = simulate_gamma
+  n, p = x.shape
+  D = torch.utils.data
+  # Important: data must be float
+  loader = D.DataLoader(
+    D.TensorDataset(torch.tensor(x, dtype=torch.float), torch.tensor(s, dtype=torch.float)),
+    batch_size=n, pin_memory=True)
+  m = scmodes.sgd.PoissonGamma(p)
+  m.fit(loader, max_epochs=2000)
+
+  log_mu_hat, log_phi_hat = m.opt()
+  assert log_mu_hat.shape == (1, p)
+  assert log_phi_hat.shape == (1, p)
+  l1 = st.nbinom(n=np.exp(-log_phi_hat), p=1 / (1 + s.dot(np.exp(log_mu_hat + log_phi_hat)))).logpmf(x).sum()
+  assert l1 > l0
+
+def test_PoissonGamma_minibatch(simulate_gamma):
+  x, s, log_mu, log_phi, l0 = simulate_gamma
+  n, p = x.shape
+  D = torch.utils.data
+  # Important: data must be float
+  loader = D.DataLoader(
+    D.TensorDataset(torch.tensor(x, dtype=torch.float), torch.tensor(s, dtype=torch.float)),
+    batch_size=100, pin_memory=True)
+  m = scmodes.sgd.PoissonGamma(p)
+  m.fit(loader, max_epochs=500)
+
+  log_mu_hat, log_phi_hat = m.opt()
+  assert log_mu_hat.shape == (1, p)
+  assert log_phi_hat.shape == (1, p)
+  l1 = st.nbinom(n=np.exp(-log_phi_hat), p=1 / (1 + s.dot(np.exp(log_mu_hat + log_phi_hat)))).logpmf(x).sum()
+  assert l1 > l0
+
+def test_PoissonGamma_sgd(simulate_gamma):
   x, s, log_mu, log_phi, l0 = simulate_gamma
   n, p = x.shape
   D = torch.utils.data
@@ -44,7 +78,9 @@ def test_PoissonGamma_fit(simulate_gamma):
     D.TensorDataset(torch.tensor(x, dtype=torch.float), torch.tensor(s, dtype=torch.float)),
     batch_size=1, pin_memory=True)
   m = scmodes.sgd.PoissonGamma(p)
-  m.fit(loader, max_epochs=10)
+  # Important: learning rate has to lowered to compensate for increased
+  # variance in gradient estimator
+  m.fit(loader, max_epochs=10, lr=5e-3)
 
   log_mu_hat, log_phi_hat = m.opt()
   assert log_mu_hat.shape == (1, p)