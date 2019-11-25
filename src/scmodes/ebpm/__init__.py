aeed71bde97c34ddfa174b4c07152e7a396c7614
Abhishek Sarkar <aksarkar@alum.mit.edu>
2019-11-25 14:54:53 -0600

    Revise scmodes.ebpm docs

--- src/scmodes/ebpm/__init__.py
+++ src/scmodes/ebpm/__init__.py
@@ -6,7 +6,13 @@ x_{ij} ~ Poisson(s_i \lambda_{ij})
 \lambda_{ij} ~ g_j(.)
 
 For g_j a point mass, or a member of the family of Gamma distributions, or
-point-Gamma distributions, the marginal likelihood is analytic.
+point-Gamma distributions, the marginal likelihood is analytic. Other choices
+of g_j require specialized inference algorithms.
+
+We provide simple implementations for analyzing a single gene at a time under
+scmodes.ebpm. We additionally provide specialized implementations of ebpm_gamma
+and ebpm_point_gamma for fitting many EBPM problems in parallel on a GPU in
+scmodes.ebpm.sgd (which requires an explicit import).
 
 """
 from .wrappers import *
