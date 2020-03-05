db2768cfe293525f9ecf92281391f8e53546c5eb
Abhishek Sarkar <aksarkar@alum.mit.edu>
2020-03-04 22:13:31 -0600

    Set default tolerances for benchmarks

--- src/scmodes/benchmark/imputation.py
+++ src/scmodes/benchmark/imputation.py
@@ -24,26 +24,14 @@ def imputation_score_ebpm_point(x, frac=0.1, seed=0, **kwargs):
   mu = (w * x).sum(axis=0) / s.sum()
   return _pois_loss(x, w, np.outer(s, mu))
 
-def imputation_score_wnmf(x, rank=10, frac=0.1, seed=0, **kwargs):
+def imputation_score_wnmf(x, rank=10, frac=0.1, tol=1e-4, max_iters=100000, seed=0, **kwargs):
   w = _mask_entries(x, frac=frac, seed=seed)
-  l, f, _ = scmodes.lra.nmf(x, w=w, rank=rank)
+  l, f, _ = scmodes.lra.nmf(x, w=w, rank=rank, tol=tol, max_iters=max_iters)
   return _pois_loss(x, w, l.dot(f.T))
 
-def imputation_score_wglmpca(x, rank=10, frac=0.1, seed=0, max_retries=10, max_iters=5000, **kwargs):
+def imputation_score_wglmpca(x, rank=10, frac=0.1, tol=1e-4, max_iters=100000, seed=0, **kwargs):
   w = _mask_entries(x, frac=frac, seed=seed)
-  opt = None
-  obj = np.inf
-  for i in range(max_retries):
-    try:
-      l, f, loss = scmodes.lra.glmpca(x, w=w, rank=rank, max_iters=max_iters)
-      if loss < obj:
-        opt = l, f
-        obj = loss
-    except RuntimeError:
-      pass
-  if opt is None:
-    raise RuntimeError('failed to converge after max_retries restarts')
-  l, f = opt
+  l, f, _ = scmodes.lra.glmpca(x, w=w, rank=rank, tol=tol, max_iters=max_iters)
   return _pois_loss(x, w, np.exp(l.dot(f.T)))
 
 def imputation_score_wnbmf(x, rank=10, frac=0.1, seed=0, inv_disp=1, fix_inv_disp=False, **kwargs):
@@ -51,10 +39,10 @@ def imputation_score_wnbmf(x, rank=10, frac=0.1, seed=0, inv_disp=1, fix_inv_dis
   l, f, loss = scmodes.lra.nbmf(x, w=w, rank=rank, inv_disp=inv_disp, fix_inv_disp=fix_inv_disp)
   return _pois_loss(x, w, l.dot(f.T))
 
-def evaluate_imputation(x, methods, n_trials=1, **kwargs):
+def evaluate_imputation(x, methods, n_trials=1, rank=10, **kwargs):
   result = []
   for m in methods:
     for i in range(n_trials):
-      score = getattr(sys.modules[__name__], f'imputation_score_{m}')(x, seed=i, **kwargs)
+      score = getattr(sys.modules[__name__], f'imputation_score_{m}')(x, seed=i, rank=rank, **kwargs)
       result.append((m, i, score))
   return pd.DataFrame(result, columns=['method', 'trial', 'loss'])
