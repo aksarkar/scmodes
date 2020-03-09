bca6192912b49a952ca962b2c8eded962512cb0a
Abhishek Sarkar <aksarkar@alum.mit.edu>
2020-03-09 12:00:35 -0500

    Unify thinning and imputation analysis code

--- src/scmodes/benchmark/imputation.py
+++ src/scmodes/benchmark/imputation.py
@@ -4,6 +4,7 @@ import pandas as pd
 import scipy.stats as st
 import scmodes
 import sys
+import torch
 
 def _mask_entries(x, frac, seed):
   np.random.seed(seed)
@@ -24,20 +25,27 @@ def imputation_score_ebpm_point(x, frac=0.1, seed=0, **kwargs):
   mu = (w * x).sum(axis=0) / s.sum()
   return _pois_loss(x, w, np.outer(s, mu))
 
-def imputation_score_wnmf(x, rank=10, frac=0.1, tol=1e-4, max_iters=100000, seed=0, **kwargs):
+def imputation_score_nmf(x, rank=10, frac=0.1, tol=1e-4, max_iters=100000, seed=0, **kwargs):
   w = _mask_entries(x, frac=frac, seed=seed)
   l, f, _ = scmodes.lra.nmf(x, w=w, rank=rank, tol=tol, max_iters=max_iters)
   return _pois_loss(x, w, l.dot(f.T))
 
-def imputation_score_wglmpca(x, rank=10, frac=0.1, tol=1e-4, max_iters=100000, seed=0, **kwargs):
+def imputation_score_glmpca(x, rank=10, frac=0.1, tol=1e-4, max_iters=100000, seed=0, **kwargs):
   w = _mask_entries(x, frac=frac, seed=seed)
   l, f, _ = scmodes.lra.glmpca(x, w=w, rank=rank, tol=tol, max_iters=max_iters)
   return _pois_loss(x, w, np.exp(l.dot(f.T)))
 
-def imputation_score_wnbmf(x, rank=10, frac=0.1, tol=1e-4, max_iters=100000, inv_disp=1, fix_inv_disp=False, seed=0, **kwargs):
+def imputation_score_nbmf(x, rank=10, frac=0.1, tol=1e-4, max_iters=100000, inv_disp=1, fix_inv_disp=False, seed=0, **kwargs):
   w = _mask_entries(x, frac=frac, seed=seed)
   l, f, inv_disp, loss = scmodes.lra.nbmf(x, w=w, rank=rank, tol=tol, max_iters=max_iters, inv_disp=inv_disp, fix_inv_disp=fix_inv_disp)
   return _pois_loss(x, w, l.dot(f.T))
+
+def imputation_score_pvae(x, rank=10, frac=0.1, lr=1e-3, max_epochs=200, seed=0, **kwargs):
+  w = _mask_entries(x, frac=frac, seed=seed)
+  n, p = x.shape
+  x = torch.tensor(x, dtype=torch.float)
+  m = scmodes.lra.PVAE(p, rank).fit(x, lr=lr, max_epochs=max_epochs)
+  return _pois_loss(x, w, m.denoise(x))
 
 def evaluate_imputation(x, methods, n_trials=1, rank=10, **kwargs):
   result = []
--- tests/benchmark/test_imputation.py
+++ tests/benchmark/test_imputation.py
@@ -14,26 +14,32 @@ def test_imputation_score_ebpm_point(simulate):
   assert np.isfinite(loss)
   assert loss > 0
 
-def test_imputation_score_wnmf(simulate):
+def test_imputation_score_nmf(simulate):
   x, eta = simulate
-  loss = scmodes.benchmark.imputation_score_wnmf(x)
+  loss = scmodes.benchmark.imputation_score_nmf(x)
   assert np.isfinite(loss)
   assert loss > 0
 
-def test_imputation_score_wnbmf(simulate):
+def test_imputation_score_nbmf(simulate):
   x, eta = simulate
-  loss = scmodes.benchmark.imputation_score_wnbmf(x, rank=1)
+  loss = scmodes.benchmark.imputation_score_nbmf(x, rank=1)
   assert np.isfinite(loss)
   assert loss > 0
 
-def test_imputation_score_wglmpca(simulate):
+def test_imputation_score_glmpca(simulate):
   x, eta = simulate
-  loss = scmodes.benchmark.imputation_score_wglmpca(x, rank=1)
+  loss = scmodes.benchmark.imputation_score_glmpca(x, rank=1)
+  assert np.isfinite(loss)
+  assert loss > 0
+
+def test_imputation_score_pvae(simulate):
+  x, eta = simulate
+  loss = scmodes.benchmark.imputation_score_pvae(x, rank=1)
   assert np.isfinite(loss)
   assert loss > 0
   
 def test_evaluate_imputation(simulate):
   x, eta = simulate
-  result = scmodes.benchmark.evaluate_imputation(x, methods=['oracle', 'wnmf', 'wglmpca'], rank=1, n_trials=1)
+  result = scmodes.benchmark.evaluate_imputation(x, methods=['oracle', 'nmf', 'glmpca'], rank=1, n_trials=1)
   assert result.shape == (3, 3)
   assert np.isfinite(result['loss']).all()
