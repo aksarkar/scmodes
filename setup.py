67f3e143344e2a751f10c0618bd0ef2ef620a70a
Abhishek Sarkar <aksarkar@alum.mit.edu>
2021-02-16 15:32:31 -0600

    Version 0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
   name='scmodes',
   description='Investigation of single cell modes',
-  version='0.8',
+  version='0.9',
   url='https://www.github.com/aksarkar/scmodes',
   author='Abhishek Sarkar',
   author_email='aksarkar@uchicago.edu',
